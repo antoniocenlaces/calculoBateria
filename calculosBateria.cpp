87325708d26d4e2228e6d58431447fd1fde3c928
antoniocenlaces <antoniocenlaces@gmail.com>
2023-05-09 11:41:54 +0000

    primeros pasos

--- calculosBateria.cpp
+++ calculosBateria.cpp
@@ -39,15 +39,38 @@ int main() {
     unsigned dia = 0; // Día del año (entre 1 y 366)
     unsigned hora = 0; // Hora del día que estoy leyendo el balance
     string dato;
-    while (getline(f, dato)) {
-        balance = stod(dato);
-        dia++; // Paso al siguiente dia
+    // f viene de windows y tirne tres bytes al inicio que hay que descartar
+    f.seekg(3);
+
+    // if (getline(f, dato, ';')) {
+    //     dia++;
+    //     string bueno;
+    //     unsigned d = 0;
+    //     for (unsigned i = 0;i < dato.length(); i++) {
+    //         if (dato[i]==46 || (dato[i]>= 48 && dato[i] <= 57)){
+    //         bueno[d]=dato[i];
+    //         d++;
+    //         }
+    //     }
+    //     balance = stod(bueno);
+    //     bateria[1].carga=balance;
+    //     hora++;
+    // }
+    while (getline(f, dato, ';') && dia < 50) {
+         dia++; // Paso al siguiente dia
+        balance = std::stod(dato);
         bateria[dia].carga = balance;
         cout << "Dia: " << dia << " hora: " << hora << " leido: " << dato << " double: " << balance << endl;
-        hora = hora == 23 ? 0 : hora++;
+        hora++;
+        if (hora == 24) {
+            cout << endl;
+            hora=0;
+            }
+        
     }
     f.close();
     for (unsigned i = 0;i <= dia; i++){
-        cout << bateria[i].carga;
+        cout << bateria[i].carga << endl;
     }
+    // cout << fixed << bateria[0].carga << endl;;
 }
