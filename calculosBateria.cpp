bc541747b844c9e5a8b89cb8e92589d38efd2f1e
antoniocenlaces <antoniocenlaces@gmail.com>
2023-05-09 08:19:26 +0000

    Inicio proyecto

---
+++ calculosBateria.cpp
@@ -0,0 +1,53 @@
+/*********************************************************************************************\
+ * Programación 1. Trabajo obligatorio
+ * Autores: Antonio José González Almela
+ * Ultima revisión: 10/12/2022
+ * Resumen: Fichero de implementación del módulo principal que implementa el trabajo
+ *          obligatorio de Programación 1 del curso 2022-23.
+\*********************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    struct tpBateria {
+        double carga; // carga al final del día de la batería en KWh
+        double aConsumo; // consumo enviado, durante el día, desde la batería al cliente en KWh
+        double compraMercado; // compra en mercado de energía, durnate este día en KWh
+        double aRed; // energía vertida a red por no entrar en batería ni en cliente en KWh
+
+    };
+    tpBateria bateria[367];
+    // bateria[0] es el estado inicial de la batería antes de comenzar con el primer día
+    // de análisis de carga/descarga
+    bateria[0].carga=1200.0;
+    string ficheroBalance;
+    cout << "Ruta del fichero con el balance energético día a día: ";
+    cin >> ficheroBalance;
+    ifstream f;
+    f.open(ficheroBalance);
+    if (!f.is_open()) {
+        cerr << "\x1b[1;31m" << "ERROR AL ABRIR EL FICHERO: \"" << ficheroBalance << "\". "
+            << "\x1b[0m" << endl;
+        return false;
+    }
+    double balance = 0.0; // Balance energético de un día leido de fichero
+    unsigned dia = 0; // Día del año (entre 1 y 366)
+    unsigned hora = 0; // Hora del día que estoy leyendo el balance
+    string dato;
+    while (getline(f, dato)) {
+        balance = stod(dato);
+        dia++; // Paso al siguiente dia
+        bateria[dia].carga = balance;
+        cout << "Dia: " << dia << " hora: " << hora << " leido: " << dato << " double: " << balance << endl;
+        hora = hora == 23 ? 0 : hora++;
+    }
+    f.close();
+    for (unsigned i = 0;i <= dia; i++){
+        cout << bateria[i].carga;
+    }
+}
